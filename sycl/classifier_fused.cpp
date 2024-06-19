88f275cf3612e88a6f0e83733d0e69a8bda594f0
mspronesti <massimiliano.pronesti@gmail.com>
2024-06-19 18:57:53 +0100

    [sycl] classifier fused kernel 5

--- sycl/classifier_fused.cpp
+++ sycl/classifier_fused.cpp
@@ -158,7 +158,7 @@ SoftmaxParams prepare_softmax_blockwide(sycl::nd_item<1> id, sycl::sub_group& wa
     float thread_sumval = 0.0f;
     // do the loop in reverse to maximise probability of L2 cache hits
     // so even small L2s get some hits on the 2nd read of the same thread
-    for (int i = (V+3)/4 + id.get_local_linear_id() - id.get_local_range(0); i >= 0; i -= id.get_local_range(0)) {
+    for (int i = ceil_div(V, 4) + id.get_local_linear_id() - id.get_local_range(0); i >= 0; i -= id.get_local_range(0)) {
         sycl::float4 v4 = x_vec4[i];
         #pragma unroll
         for(int k = 0; k < 4; k++) {
@@ -368,7 +368,114 @@ void fused_classifier_kernel4(sycl::nd_item<1> id, float* dlogits, float* losses
     }
 }
 
+SoftmaxParams prepare_softmax_blockwide3(sycl::nd_item<1> id, sycl::sub_group& warp,
+                                         int idx, const float* inp, int V, int P) {
+    // same but not float4
+    // one row of inp, i.e. inp[idx, :] of shape (V,)
+    const floatX* x = inp + idx * P;
+    float thread_maxval = -INFINITY;
+    float thread_sumval = 0.0f;
+    int i = (V+x128::size-1)/x128::size + id.get_local_linear_id() - id.get_local_range(0);
 
+    // special-case loop to handle the unaligned elements at the end of the array
+    // this lets us skip the bounds check in the main loop below, which improves performance
+    while ((i+1)*x128::size > V) {
+        for(int k = 0; k < x128::size; ++k) {
+            if (i*x128::size+k >= V) {
+                break; // bounds checking against real V (rather than padded P)
+            }
+            float v = (float)x[i*x128::size+k];
+            float old_maxval = thread_maxval;
+            thread_maxval = sycl::fmax(thread_maxval, v);
+            thread_sumval *= sycl::exp((old_maxval - thread_maxval));
+            thread_sumval += sycl::exp(v - thread_maxval);
+        }
+        i -= id.get_local_range(0);
+    }
+
+    // main loop for the bulk of the iterations (no bounds checking required!)
+    for (; i >= 0; i -= id.get_local_range(0)) {
+        x128 packed_x = load128(x + i * x128::size); // load and keep in cache until fused_classifier loop
+        for(int k = 0; k < x128::size; ++k) {
+            float v = (float)packed_x[k];
+            float old_maxval = thread_maxval;
+            thread_maxval = sycl::fmax(thread_maxval, v);
+            thread_sumval *= sycl::exp((old_maxval - thread_maxval));
+            thread_sumval += sycl::exp(v - thread_maxval);
+        }
+    }
+
+    // Block Max Reduction -> Maths -> Block Sum Reduction
+    float block_maxval = sycl::reduce_over_group(id.get_group(), thread_maxval, sycl::maximum<float>{});
+    thread_sumval *= sycl::exp(thread_maxval - block_maxval);
+    float block_sumval = sycl::reduce_over_group(id.get_group(), thread_sumval, sycl::plus<float>{});
+    // return the softmax parameters
+    return SoftmaxParams{1.f / block_sumval, block_maxval};
+}
+
+// will _update_ logits to logit gradients
+// uses template to decide whether to write logits and probs
+// split both loops in "multiple-of-x128-size" and "bounds-checked remainder" parts
+template <bool WriteLogits = true, bool WriteProbs = false>
+void fused_classifier_kernel5(sycl::nd_item<1> id, floatX* dlogits, floatX* losses, floatX* probs,
+                              const floatX* logits, const floatX* dlosses, const int* targets,
+                              int B, int T, int V, int P){
+    sycl::sub_group warp = id.get_sub_group();
+    int idx = id.get_group(0);
+    int ix = targets[idx];
+
+    // softmax (reading B * T * V, same logits read again below, hopefully still in cache)
+    SoftmaxParams sp = prepare_softmax_blockwide3(id, warp, idx, logits, V, P);
+
+    // calculate the probability needed for the loss and update (single-threaded)
+    if(id.get_group().leader()) {
+        float prob = expf(logits[idx * P + ix] - sp.Offset) * sp.Scale;
+        losses[idx] = (floatX)(-logf(prob));
+    }
+
+    // very sensible default for dlosses is 1/(B*T), which is the uniform loss
+    float dloss = dlosses != nullptr ? dlosses[idx] : 1.0f / (B*T);
+    // calculate the gradients directly, saves bandwidth from probs during training
+    // but also supports writing probs for inference-only and debugging
+    const float* logits_vec = logits + idx * P;
+    for (int i = id.get_local_linear_id(); i < V/x128::size; i += id.get_local_range(0)) {
+        // this is the 2nd read of logits after the one in prepare_softmax2
+        // it will be overwritten by the logits gradients which is when we reduce cache persistence
+        x128 packed_logits_vec = load128(logits_vec + i * x128::size); // rely on cs of store128cs
+        x128 packed_probs;
+        for(int k = 0; k < x128::size; ++k) {
+            int element = i*x128::size + k;
+            float prob = expf((float)packed_logits_vec[k] - sp.Offset) * sp.Scale;
+            packed_probs[k] = (floatX)prob;
+            float indicator = (element == ix) ? 1.0f : 0.0f;
+            packed_logits_vec[k] = (floatX)((prob - indicator) * dloss);
+        }
+        if (WriteLogits){
+            // reduce cache persistence for the overwritten logits
+            // to maximise probability that logits remain in cache between prepare_softmax and here
+            store128cs(dlogits + idx * P + i * x128::size, packed_logits_vec);
+        }
+        if (WriteProbs) {
+            store128(probs + idx * P + i * x128::size, packed_probs);
+        }
+    }
+
+    // handle remaining elements after the last multiple of x128::size
+    // e.g. if V = 8003, and x128::size = 8, we need to handle the last 3 elements
+    int unaligned_start = V & ~(x128::size - 1); // round down to multiple of x128::size
+    for (int i = id.get_local_linear_id() + unaligned_start; i < V; i++) {
+        float prob = expf((float)logits_vec[i] - sp.Offset) * sp.Scale;
+        float indicator = (i == ix) ? 1.0f : 0.0f;
+        float dlogit = (prob - indicator) * dloss;
+        if (WriteLogits){
+            // __stcs(dlogits + idx * P + i, (floatX)dlogit);
+            dlogits[idx * P + i] = (floatX)dlogit;
+        }
+        if (WriteProbs) {
+            probs[idx * P + i] = (floatX)prob;
+        }
+    }
+}
 // ----------------------------------------------------------------------------
 // kernel launcher
 
@@ -420,6 +527,20 @@ void fused_classifier4(sycl::queue &q, float* dlogits, float* losses,
     }).wait();
 }
 
+void fused_classifier5(sycl::queue &q, float* dlogits, float* losses,
+                       const float* logits, const float* dlosses, const int* targets,
+                       int B, int T, int V, int P, int block_size) {
+    const int N = B * T;
+    const int grid_size = N;
+
+    q.parallel_for(sycl::nd_range<1>(grid_size * block_size, block_size), [=]
+            // this tries to port __launch_bounds__(1024, MAX_1024_THREADS_BLOCKS)
+            [[intel::max_work_group_size(1, 1, 1024),
+              intel::min_work_groups_per_cu(2)]](sycl::nd_item<1> id){
+        fused_classifier_kernel5<true, false>(id, (floatX*)dlogits, (floatX*)losses, nullptr, (floatX*)logits, (floatX*)dlosses, targets, B, T, V, P);
+    }).wait();
+}
+
 
 void fused_classifier(int kernel_num, sycl::queue &q, float* dlogits, float* losses,
                       const float* logits, const float* dlosses, const int* targets,
@@ -436,6 +557,9 @@ void fused_classifier(int kernel_num, sycl::queue &q, float* dlogits, float* los
             break;
         case 4:
             fused_classifier4(q, dlogits, losses, logits, dlosses, targets, B, T, V, P, block_size);
+            break;
+        case 5:
+            fused_classifier5(q, dlogits, losses, logits, dlosses, targets, B, T, V, P, block_size);
             break;
         default:
             std::cout << "Invalid kernel number\n";
