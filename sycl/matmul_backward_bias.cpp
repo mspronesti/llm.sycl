988dc79efa82538409b967e7611b37a3c7eca8eb
mspronesti <massimiliano.pronesti@gmail.com>
2024-07-10 18:40:50 +0100

    [sycl] matmul bwd bias kern 7

--- sycl/matmul_backward_bias.cpp
+++ sycl/matmul_backward_bias.cpp
@@ -48,6 +48,7 @@ void matmul_backward_bias_cpu(float* dinp, float* dweight, float* dbias,
 
 // ----------------------------------------------------------------------------
 // GPU kernels
+float* dbias_buffer;
 
 void matmul_backward_bias_kernel1(sycl::nd_item<1> id, floatX* dbias, const floatX* dout, int B, int T, int OC) {
     int o = id.get_group(0); // range [0, OC)
@@ -185,6 +186,72 @@ void matmul_backward_bias_kernel5(sycl::nd_item<2> id, floatX* dbias, const floa
 }
 #endif
 
+void cast_and_add_kernel(sycl::nd_item<1> id, floatX* dst, const float* src, size_t n) {
+    // used only for matmul_backward_bias kernel, a little bit embarassing TODO delete later
+    const size_t idx = id.get_global_id(0);
+    if (idx < n) { dst[idx] = (floatX)((float)dst[idx] + src[idx]); } // have to += because dbias is a paramater
+}
+
+void matmul_backward_bias_kernel7(sycl::nd_item<2> id, float* dbias, const floatX* dout, int B, int T, int OC, const int block_size,
+                                  sycl::local_accessor<float> local_acc) {
+    // note: this kernel reads in floatX, but it writes to float!
+    // this is because we're using atomics, which are super slow in < fp32 precision on < H100 GPUs
+    // so the trick is do fp32 atomics to a buffer, and then copy_and_cast the result to floatX
+    // (this also results in higher accuracy than doing accumulation directly in floatX)
+
+    // see comments in matmul_backward() for an explanation of block/grid dimensions etc.
+    const int block_size_x = 32;
+    const int block_size_y = block_size / block_size_x; // 16
+    const int OC_per_warp = block_size_x * x128::size;  // 256 at BF16
+
+    int threadIdx_x = id.get_local_id(1);
+    int threadIdx_y = id.get_local_id(0);
+    int blockIdx_x = id.get_group(1);
+    int blockIdx_y = id.get_group(0);
+
+    int local_oc = threadIdx_x * x128::size;
+    int global_oc = id.get_group(1) * OC_per_warp + local_oc;
+    float accumulators[x128::size];
+
+    float* shared = local_acc.get_multi_ptr<sycl::access::decorated::no>().get_raw();
+
+    for (int k = 0; k < x128::size; k++) {
+        accumulators[k] = 0.0f;
+    }
+    int thread_id = threadIdx_y * block_size_x + threadIdx_x;
+    for (int idx = thread_id; idx < OC_per_warp; idx += block_size) {
+        shared[idx] = 0.0f;
+    }
+    sycl::group_barrier(id.get_group());
+    if(global_oc < OC) {
+        for (int idx = blockIdx_y*block_size_y + threadIdx_y; idx < B * T; idx += id.get_group_range(0)*block_size_y) {
+            x128 packed_dout = load128(dout + global_oc + idx*OC);
+            for (int k = 0; k < x128::size; k++) {
+                accumulators[k] += (float)packed_dout[k];
+            }
+        }
+        // we need to avoid shared memory bank conflicts for the atomicAdd to maximise performance,
+        // so we accumulate in a conflict-free order, then reorder to match the global memory order
+        for (int k = 0; k < x128::size; k++) {
+            atomicAdd(shared + threadIdx_x + (k * block_size_x), accumulators[k]);
+        }
+    }
+    if (threadIdx_y >= x128::size) { return; } // only need this many warps to reorder the data
+    sycl::group_barrier(id.get_group());
+    // read the accumulated values in the conflict-free order
+    int i = threadIdx_x + (threadIdx_y * block_size_x);
+    float tmp = shared[i];
+    sycl::group_barrier(id.get_group());
+    // write them back to shared memory in the global memory order
+    // 8-way bank conflict for BF16 x128, but only 8x per threadblock (rather than 8x per warp)
+    shared[local_oc + threadIdx_y] = tmp;
+    sycl::group_barrier(id.get_group());
+    // now we do a perfectly coalesced atomic add to global memory (1x 128-byte cacheline per warp)
+    if (i + blockIdx_x*OC_per_warp < OC) {
+        atomicAdd(dbias + i + blockIdx_x*OC_per_warp, shared[i]);
+    }
+}
+
 
 // ----------------------------------------------------------------------------
 // kernel launcher
@@ -253,6 +320,52 @@ void matmul_backward_bias5(sycl::queue &q, floatX* dbias, const floatX* dout,
 }
 #endif
 
+void matmul_backward_bias7(sycl::queue &q, floatX* dbias, const floatX* dout,
+                           int B, int T, int OC, int block_size) {
+    if(block_size < 256) {
+        block_size = 256;
+    }
+    if (block_size > 256) {
+        // larger block sizes provokes the early exit problem on Xe1.
+        block_size = 256;
+    }
+    // Each warp is responsible for 32 * "x128::size" = 256 OCs at BF16 (OC must be a multiple of 256!)
+    // Block size is 512 threads (16 warps) and we reduce those 16 values into 1 at the end
+    // blockDim.x is 32 --> single warp being responsible for those 256 OCs
+    // blockDim.y is 16 --> 16 parallel independent warps processing the same OCs for different BTs
+    // gridDim.x is OC / 256 --> each block processes 256 OCs
+    // grimDim.y is max(1, (cuda_num_SMs * threads_per_SM) / (512 * gridDim.x)); --> fill up the entire GPU!
+    const int warp_size = 32;
+    const int OC_per_warp = warp_size * x128::size; // 256 at BF16
+    const int block_size_x = 32;
+    const int block_size_y = block_size / block_size_x; // 16
+    const int grid_size_x = ceil_div(OC, OC_per_warp); // e.g. 3 horizontal blocks for 768 OCs at BF16
+
+    int max_CUs = q.get_device().get_info<sycl::info::device::max_compute_units>();
+    int max_wgs = q.get_device().get_info<sycl::info::device::max_work_group_size>();
+    const int grid_size_y = std::max(1, (max_CUs * max_wgs) / block_size);
+
+    assert(block_size_y >= x128::size);
+
+    sycl::range<2> block_dim(block_size_y, block_size_x);
+    sycl::range<2> grid_dim(grid_size_y, grid_size_x);
+
+    // this is needed because dbias_buffer is a non const global variable
+    // and can't be passed to the kernel directly in SYCL
+    float* dbias_buffer_loc = dbias_buffer;
+
+    q.memset(dbias_buffer_loc, 0, OC * sizeof(float)).wait();
+    q.submit([&](sycl::handler& h) {
+        sycl::local_accessor<float> local_acc(OC_per_warp, h);
+        h.parallel_for(sycl::nd_range<2>(grid_dim*block_dim, block_dim), [=](sycl::nd_item<2> id) [[sycl::reqd_sub_group_size(32)]]{
+            matmul_backward_bias_kernel7(id, dbias_buffer_loc, dout, B, T, OC, block_size, local_acc);
+        });
+    }).wait();
+
+    q.parallel_for(sycl::nd_range<1>(ceil_div(OC, 256)*256, 256), [=](sycl::nd_item<1> id){
+        cast_and_add_kernel(id, dbias, dbias_buffer_loc, OC);
+    }).wait();
+}
 
 void matmul_backward_bias(int kernel_num,
                           sycl::queue &q,
@@ -276,6 +389,9 @@ void matmul_backward_bias(int kernel_num,
             matmul_backward_bias5(q, dbias, dout, B, T, OC, block_size);
             break;
 #endif
+        case 7:
+            matmul_backward_bias7(q, dbias, dout, B, T, OC, block_size);
+            break;
         default:
             std::cout << "Invalid kernel number\n";
             exit(1);
@@ -313,6 +429,7 @@ int main(int argc, char **argv) {
 
     d_dbias = sycl::malloc_device<floatX>(OC, q);
     d_dout = sycl::malloc_device<floatX>(B * T * OC, q);
+    dbias_buffer = sycl::malloc_device<float>(OC * 32, q);
 
     memcpy_convert(d_dbias, dbias, OC, q);
     memcpy_convert(d_dout, dout, B * T * OC, q);
@@ -349,6 +466,7 @@ int main(int argc, char **argv) {
     free(dout);
     sycl::free(d_dbias, q);
     sycl::free(d_dout, q);
+    sycl::free(dbias_buffer, q);
 
     return 0;
 }
